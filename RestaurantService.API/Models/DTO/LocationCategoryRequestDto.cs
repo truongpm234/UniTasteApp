677faf11e951bd5479007d1505548d4f69ca4d5f
truongpm234 <phamminhtruong2032004@gmail.com>
2025-10-09 23:16:15 +0700

    fix and add api search

---
+++ RestaurantService.API/Models/DTO/LocationCategoryRequestDto.cs
@@ -0,0 +1,11 @@
+﻿namespace RestaurantService.API.Models.DTO
+{
+    public class LocationCategoryRequestDto
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public double RadiusKm { get; set; }
+        public string CategoryName { get; set; }
+    }
+
+}
