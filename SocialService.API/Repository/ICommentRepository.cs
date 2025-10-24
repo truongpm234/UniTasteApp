92a49e11be0fce67adf27ef9df197eaa91e1aea3
truongpm234 <phamminhtruong2032004@gmail.com>
2025-10-24 11:09:59 +0700

    commit

--- SocialService.API/Repository/ICommentRepository.cs
+++ SocialService.API/Repository/ICommentRepository.cs
@@ -4,6 +4,7 @@ namespace SocialService.API.Repository
 {
     public interface ICommentRepository
     {
+        Task<Comment?> GetCommentByPostIdAsync(int postId);
         Task AddComment(Comment comment);
         Task SaveAsync();
     }
